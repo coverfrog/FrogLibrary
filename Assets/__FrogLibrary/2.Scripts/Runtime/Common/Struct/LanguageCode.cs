dc9c192d8121b1f2664149ad9a8c840949728a80
coverfrog <justfroglab@gmail.com>
2026-03-08 14:30:45 +0900

    언어 코드

---
+++ Assets/__FrogLibrary/2.Scripts/Runtime/Common/Struct/LanguageCode.cs
@@ -0,0 +1,15 @@
+﻿namespace FrogLibrary
+{
+    public enum LanguageCode
+    {
+        Kr,
+        En,
+        Fr,
+    }
+
+    public enum SupportedLanguageCode
+    {
+        Kr = LanguageCode.Kr,
+        En = LanguageCode.En,
+    }
+}
